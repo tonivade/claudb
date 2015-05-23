915e36ac6db3adc851737e6ab3a4731278b92394
Antonio Muñoz <antoniogmc@gmail.com>
2015-05-23 18:31:19 +0200

    new commands, mset and getset.

--- src/main/tonivade/db/data/DatabaseValue.java
+++ src/main/tonivade/db/data/DatabaseValue.java
@@ -75,6 +75,42 @@ public class DatabaseValue {
         return i;
     }
 
+
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DatabaseValue other = (DatabaseValue) obj;
+        if (type != other.type)
+            return false;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+
     public static DatabaseValue string(String value) {
         return new DatabaseValue(DataType.STRING, value);
     }
--- src/test/tonivade/db/command/impl/SetCommandTest.java
+++ src/test/tonivade/db/command/impl/SetCommandTest.java
@@ -1,15 +1,11 @@
 package tonivade.db.command.impl;
 
-import static org.mockito.Matchers.argThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static tonivade.db.data.DatabaseValue.string;
 
-import java.util.function.BiFunction;
-
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -17,8 +13,6 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import tonivade.db.command.IRequest;
 import tonivade.db.command.IResponse;
-import tonivade.db.data.DataType;
-import tonivade.db.data.DatabaseValue;
 import tonivade.db.data.IDatabase;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -42,34 +36,7 @@ public class SetCommandTest {
 
         command.execute(db, request, response);
 
-        verify(db).merge(
-                eq("a"),
-                argThat(new BaseMatcher<DatabaseValue>() {
-                            @Override
-                            public void describeTo(Description description) {
-                                description.appendText("value expected: \"1\"");
-                            }
-
-                            @Override
-                            public boolean matches(Object value) {
-                                DatabaseValue db = (DatabaseValue) value;
-                                return db.getType() == DataType.STRING && db.getValue().equals("1");
-                            }
-                        }),
-                argThat(new BaseMatcher<BiFunction<DatabaseValue, DatabaseValue, DatabaseValue>>() {
-                            @Override
-                            public void describeTo(Description description) {
-                                description.appendText("value expected: \"1\"");
-                            }
-
-                            @Override
-                            public boolean matches(Object value) {
-                                BiFunction<DatabaseValue, DatabaseValue, DatabaseValue> function =
-                                        (BiFunction<DatabaseValue, DatabaseValue, DatabaseValue>) value;
-                                DatabaseValue db = function.apply(string("0"), string("1"));
-                                return db.getValue().equals("1");
-                            }
-                        }));
+        verify(db).merge(eq("a"), eq(string("1")), any());
         verify(response).addSimpleStr("OK");
     }
 
