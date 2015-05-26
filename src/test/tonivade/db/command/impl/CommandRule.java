c3ad6a24fdead94befc9e89996c3630d4f101826
Antonio Muñoz <antoniogmc@gmail.com>
2015-05-26 08:47:02 +0200

    unit tests with TestRules

---
+++ src/test/tonivade/db/command/impl/CommandRule.java
@@ -0,0 +1,67 @@
+package tonivade.db.command.impl;
+
+import java.util.HashMap;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import tonivade.db.command.ICommand;
+import tonivade.db.command.IRequest;
+import tonivade.db.command.IResponse;
+import tonivade.db.data.Database;
+import tonivade.db.data.DatabaseValue;
+import tonivade.db.data.IDatabase;
+
+public class CommandRule implements TestRule {
+
+    private IRequest request;
+
+    private IResponse response;
+
+    private IDatabase database;
+
+    private final Object target;
+
+    public CommandRule(Object target) {
+        super();
+        this.target = target;
+    }
+
+    public IRequest getRequest() {
+        return request;
+    }
+
+    public IResponse getResponse() {
+        return response;
+    }
+
+    public IDatabase getDatabase() {
+        return database;
+    }
+
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                request = Mockito.mock(IRequest.class);
+                response = Mockito.mock(IResponse.class);
+                database = new Database(new HashMap<String, DatabaseValue>());
+
+                MockitoAnnotations.initMocks(target);
+
+                base.evaluate();
+
+                database.clear();
+            }
+        };
+    }
+
+    public void execute(ICommand command) {
+        command.execute(database, request, response);
+    }
+
+}
