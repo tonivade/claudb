574657a1e345d9e730abff1711cb74ca60b6d5d3
Antonio Muñoz <antoniogmc@gmail.com>
2015-08-08 18:27:07 +0200

    SafeString comparator using byte array

--- src/main/tonivade/db/redis/SafeString.java
+++ src/main/tonivade/db/redis/SafeString.java
@@ -74,8 +74,18 @@ public class SafeString implements Comparable<SafeString> {
 
     @Override
     public int compareTo(SafeString o) {
-        // FIXME:
-        return toString().compareTo(o.toString());
+        return compare(getBytes(), o.getBytes());
+    }
+
+    private int compare(byte[] left, byte[] right) {
+        for (int i = 0, j = 0; i < left.length && j < right.length; i++, j++) {
+            int a = (left[i] & 0xff);
+            int b = (right[j] & 0xff);
+            if (a != b) {
+                return a - b;
+            }
+        }
+        return left.length - right.length;
     }
 
     @Override
