243fe542ba52951a64439d53a91e6b6aab7e8b5f
Antonio Muñoz <antonio.munoz@rumbo.com>
2017-04-04 15:00:16 +0200

    working in eval issue with jedis
    premature end of stream

--- src/test/java/com/github/tonivade/tinydb/TinyDBTest.java
+++ src/test/java/com/github/tonivade/tinydb/TinyDBTest.java
@@ -20,89 +20,109 @@ import redis.clients.jedis.Pipeline;
 
 public class TinyDBTest {
 
-    @Rule
-    public final TinyDBRule rule = new TinyDBRule();
+  @Rule
+  public final TinyDBRule rule = new TinyDBRule();
 
-    @Test
-    public void testCommands() {
-        try (Jedis jedis = createClientConnection()) {
-            assertThat(jedis.ping(), equalTo("PONG"));
-            assertThat(jedis.echo("Hi!"), equalTo("Hi!"));
-            assertThat(jedis.set("a", "1"), equalTo("OK"));
-            assertThat(jedis.strlen("a"), equalTo(1L));
-            assertThat(jedis.strlen("b"), equalTo(0L));
-            assertThat(jedis.exists("a"), equalTo(true));
-            assertThat(jedis.exists("b"), equalTo(false));
-            assertThat(jedis.get("a"), equalTo("1"));
-            assertThat(jedis.get("b"), nullValue());
-            assertThat(jedis.getSet("a", "2"), equalTo("1"));
-            assertThat(jedis.get("a"), equalTo("2"));
-            assertThat(jedis.del("a"), equalTo(1L));
-            assertThat(jedis.get("a"), nullValue());
-            assertThat(jedis.quit(), equalTo("OK"));
-        }
+  @Test
+  public void testCommands() {
+    try (Jedis jedis = createClientConnection()) {
+      assertThat(jedis.ping(), equalTo("PONG"));
+      assertThat(jedis.echo("Hi!"), equalTo("Hi!"));
+      assertThat(jedis.set("a", "1"), equalTo("OK"));
+      assertThat(jedis.strlen("a"), equalTo(1L));
+      assertThat(jedis.strlen("b"), equalTo(0L));
+      assertThat(jedis.exists("a"), equalTo(true));
+      assertThat(jedis.exists("b"), equalTo(false));
+      assertThat(jedis.get("a"), equalTo("1"));
+      assertThat(jedis.get("b"), nullValue());
+      assertThat(jedis.getSet("a", "2"), equalTo("1"));
+      assertThat(jedis.get("a"), equalTo("2"));
+      assertThat(jedis.del("a"), equalTo(1L));
+      assertThat(jedis.get("a"), nullValue());
+      assertThat(jedis.eval("return 1"), equalTo(1L));
+      assertThat(jedis.quit(), equalTo("OK"));
     }
+  }
 
-    @Test
-    public void testPipeline() {
-        try (Jedis jedis = createClientConnection()) {
-            Pipeline p = jedis.pipelined();
-            p.ping();
-            p.echo("Hi!");
-            p.set("a", "1");
-            p.strlen("a");
-            p.strlen("b");
-            p.exists("a");
-            p.exists("b");
-            p.get("a");
-            p.get("b");
-            p.getSet("a", "2");
-            p.get("a");
-            p.del("a");
-            p.get("a");
+  @Test
+  public void testPipeline() {
+    try (Jedis jedis = createClientConnection()) {
+      Pipeline p = jedis.pipelined();
+      p.ping();
+      p.echo("Hi!");
+      p.set("a", "1");
+      p.strlen("a");
+      p.strlen("b");
+      p.exists("a");
+      p.exists("b");
+      p.get("a");
+      p.get("b");
+      p.getSet("a", "2");
+      p.get("a");
+      p.del("a");
+      p.get("a");
+      p.eval("return 1");
 
-            Iterator<Object> result = p.syncAndReturnAll().iterator();
-            assertThat(result.next(), equalTo("PONG"));
-            assertThat(result.next(), equalTo("Hi!"));
-            assertThat(result.next(), equalTo("OK"));
-            assertThat(result.next(), equalTo(1L));
-            assertThat(result.next(), equalTo(0L));
-            assertThat(result.next(), equalTo(true));
-            assertThat(result.next(), equalTo(false));
-            assertThat(result.next(), equalTo("1"));
-            assertThat(result.next(), nullValue());
-            assertThat(result.next(), equalTo("1"));
-            assertThat(result.next(), equalTo("2"));
-            assertThat(result.next(), equalTo(1L));
-            assertThat(result.next(), nullValue());
+      Iterator<Object> result = p.syncAndReturnAll().iterator();
+      assertThat(result.next(), equalTo("PONG"));
+      assertThat(result.next(), equalTo("Hi!"));
+      assertThat(result.next(), equalTo("OK"));
+      assertThat(result.next(), equalTo(1L));
+      assertThat(result.next(), equalTo(0L));
+      assertThat(result.next(), equalTo(true));
+      assertThat(result.next(), equalTo(false));
+      assertThat(result.next(), equalTo("1"));
+      assertThat(result.next(), nullValue());
+      assertThat(result.next(), equalTo("1"));
+      assertThat(result.next(), equalTo("2"));
+      assertThat(result.next(), equalTo(1L));
+      assertThat(result.next(), nullValue());
+      assertThat(result.next(), equalTo(1L));
 
-            jedis.quit();
-        }
+      jedis.quit();
     }
+  }
 
-    @Test
-    public void testLoad100000() {
-        int times = 100000;
-        try (Jedis jedis = createClientConnection()) {
-            long start = System.nanoTime();
-            for (int i = 0; i < times; i++) {
-                jedis.set(key(i), value(i));
-            }
-            jedis.quit();
-            assertThat((System.nanoTime() - start) / times, lessThan(1000000L));
-        }
+  @Test
+  public void testEval() throws Exception
+  {
+    try (Jedis jedis = createClientConnection()) {
+      assertThat(jedis.eval("return 1"), equalTo(1L));
     }
+  }
 
-    private Jedis createClientConnection() {
-        return new Jedis(ITinyDB.DEFAULT_HOST, ITinyDB.DEFAULT_PORT);
+  @Test
+  public void testEvalScript() throws Exception
+  {
+    try (Jedis jedis = createClientConnection()) {
+      assertThat(jedis.eval("local keys = redis.call('keys', '*region*') for i,k in ipairs(keys) do local res = redis.call('del', k) end"),
+                 equalTo(1L));
     }
+  }
 
-    private String value(int i) {
-        return "value" + String.valueOf(i);
+  @Test
+  public void testLoad100000() {
+    int times = 100000;
+    try (Jedis jedis = createClientConnection()) {
+      long start = System.nanoTime();
+      for (int i = 0; i < times; i++) {
+        jedis.set(key(i), value(i));
+      }
+      jedis.quit();
+      assertThat((System.nanoTime() - start) / times, lessThan(1000000L));
     }
+  }
 
-    private String key(int i) {
-        return "key" + String.valueOf(i);
-    }
+  private Jedis createClientConnection() {
+    return new Jedis(ITinyDB.DEFAULT_HOST, ITinyDB.DEFAULT_PORT, 10000);
+  }
+
+  private String value(int i) {
+    return "value" + String.valueOf(i);
+  }
+
+  private String key(int i) {
+    return "key" + String.valueOf(i);
+  }
 
 }
