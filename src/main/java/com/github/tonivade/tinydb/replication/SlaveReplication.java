621349d617e5e84f93c5eac6058d246b5bef11ac
Antonio Muñoz <antonio.munoz@rumbo.com>
2017-04-10 08:14:20 +0200

    removed a redundand semicolon

--- src/main/java/com/github/tonivade/tinydb/replication/SlaveReplication.java
+++ src/main/java/com/github/tonivade/tinydb/replication/SlaveReplication.java
@@ -83,7 +83,7 @@ public class SlaveReplication implements RespCallback {
 
   private void processCommand(ArrayRedisToken token) {
     Collection<RedisToken<?>> array = token.getValue();
-    RedisToken<SafeString> commandToken = (RedisToken<SafeString>) array.stream().findFirst().orElse(nullString());;
+    RedisToken<SafeString> commandToken = (RedisToken<SafeString>) array.stream().findFirst().orElse(nullString());
     List<RedisToken<SafeString>> paramTokens = List.class.cast(array.stream().skip(1).collect(toList()));
 
     LOGGER.fine(() -> "command recieved from master: " + commandToken.getValue());
