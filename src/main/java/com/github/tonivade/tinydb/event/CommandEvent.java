438ae5248503851af72ac69e7fef5df66ce8c2f2
Antonio Muñoz <antonio.munoz@rumbo.com>
2017-09-06 12:13:00 +0200

    added licence header

--- src/main/java/com/github/tonivade/tinydb/event/CommandEvent.java
+++ src/main/java/com/github/tonivade/tinydb/event/CommandEvent.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2015-2017, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
 package com.github.tonivade.tinydb.event;
 
 import static java.lang.String.format;
