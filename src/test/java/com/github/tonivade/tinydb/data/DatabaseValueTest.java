4b7e68339830bd44f8236960ea97368b51e96215
Antonio Muñoz <antoniogmc@gmail.com>
2017-09-10 11:35:01 +0200

    tests to verify that DatabaseValues are unmodifiable

--- src/test/java/com/github/tonivade/tinydb/data/DatabaseValueTest.java
+++ src/test/java/com/github/tonivade/tinydb/data/DatabaseValueTest.java
@@ -4,13 +4,26 @@
  */
 package com.github.tonivade.tinydb.data;
 
+import static com.github.tonivade.resp.protocol.SafeString.safeString;
+import static com.github.tonivade.tinydb.data.DatabaseValue.entry;
+import static com.github.tonivade.tinydb.data.DatabaseValue.hash;
+import static com.github.tonivade.tinydb.data.DatabaseValue.list;
+import static com.github.tonivade.tinydb.data.DatabaseValue.score;
+import static com.github.tonivade.tinydb.data.DatabaseValue.set;
 import static com.github.tonivade.tinydb.data.DatabaseValue.string;
+import static com.github.tonivade.tinydb.data.DatabaseValue.zset;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableSet;
+import java.util.Set;
 
 import org.junit.Test;
+
+import com.github.tonivade.resp.protocol.SafeString;
 
 public class DatabaseValueTest {
 
@@ -35,11 +48,46 @@ public class DatabaseValueTest {
     assertThat(expiredValue.timeToLiveSeconds(now), is(10));
     assertThat(expiredValue.timeToLiveMillis(now), is(10000L));
 
-
     Instant expired = now.plusSeconds(11);
 
     assertThat(expiredValue.isExpired(expired), is(true));
     assertThat(expiredValue.timeToLiveMillis(expired), is(-1000L));
     assertThat(expiredValue.timeToLiveSeconds(expired), is(-1));
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testSetUnmodifiable() {
+    DatabaseValue value = set(safeString("a"), safeString("b"), safeString("c"));
+
+    Set<SafeString> list = value.getValue();
+
+    list.add(safeString("d"));
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testListUnmodifiable() {
+    DatabaseValue value = list(safeString("a"), safeString("b"), safeString("c"));
+
+    List<SafeString> list = value.getValue();
+
+    list.add(safeString("d"));
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testHashUnmodifiable() {
+    DatabaseValue value = hash(entry(safeString("a"), safeString("1")));
+
+    Map<SafeString, SafeString> hash = value.getValue();
+
+    hash.put(safeString("d"), safeString("3"));
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void testSortedSetUnmodifiable() {
+    DatabaseValue value = zset(score(1.0, safeString("a")), score(2.0, safeString("b")), score(3.0, safeString("c")));
+
+    NavigableSet<Map.Entry<Double, SafeString>> sortedSet = value.getValue();
+
+    sortedSet.add(score(1.0, safeString("d")));
   }
 }
