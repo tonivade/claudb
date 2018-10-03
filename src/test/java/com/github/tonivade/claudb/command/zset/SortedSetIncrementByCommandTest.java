f8dfff06d3017e34baae2db245df3fe4e2669c3a
Antonio Muñoz <antonio.munoz@rumbo.com>
2018-10-03 15:56:29 +0200

    open version 1.4.0

--- src/test/java/com/github/tonivade/claudb/command/zset/SortedSetIncrementByCommandTest.java
+++ src/test/java/com/github/tonivade/claudb/command/zset/SortedSetIncrementByCommandTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2015-2018, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
 package com.github.tonivade.claudb.command.zset;
 
 import static com.github.tonivade.claudb.data.DatabaseValue.score;
