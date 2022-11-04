653292aeaad277442a98047b39c4186ef6468d0c
Antonio Muñoz <antoniogmc@gmail.com>
2022-11-04 22:56:38 +0100

    fix build

--- lib/src/test/java/com/github/tonivade/claudb/persistence/RDBOutputStreamTest.java
+++ lib/src/test/java/com/github/tonivade/claudb/persistence/RDBOutputStreamTest.java
@@ -69,7 +69,7 @@ public class RDBOutputStreamTest {
   public void testLargeString() throws IOException {
     out.dabatase(database().add(safeKey("a"), string(readFile("../README.md"))).build());
 
-    assertThat(toHexString(baos.toByteArray()), startsWith("0001615a5c"));
+    assertThat(toHexString(baos.toByteArray()), startsWith("0001615ad9"));
   }
 
   @Test
@@ -81,7 +81,7 @@ public class RDBOutputStreamTest {
     }
     out.dabatase(database().add(safeKey("a"), string(result)).build());
 
-    assertThat(toHexString(baos.toByteArray()), startsWith("0001618000010798"));
+    assertThat(toHexString(baos.toByteArray()), startsWith("0001618000010c7a"));
   }
 
   @Test
