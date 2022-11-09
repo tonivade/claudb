5e683eae2da4e2da6e2b769a7d1eb0d4b3e9b0d7
Antonio Muñoz <antoniogmc@gmail.com>
2022-11-09 11:37:55 +0100

    update jmh tests

--- lib/src/jmh/java/com/github/tonivade/claudb/ClauDBBenchmark.java
+++ lib/src/jmh/java/com/github/tonivade/claudb/ClauDBBenchmark.java
@@ -4,18 +4,34 @@
  */
 package com.github.tonivade.claudb;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import com.github.tonivade.resp.RespServer;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Pipeline;
 
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(5)
 public class ClauDBBenchmark {
+  
+  private static RespServer server = ClauDB.builder().build();
+  
+  static {
+    server.start();
+  }
   
   private Jedis jedis = new Jedis("localhost", 7081);
   
