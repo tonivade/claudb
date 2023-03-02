a69586c19ec824203b2113a1d4c6646a08c01c1f
Antonio Muñoz <antoniogmc@gmail.com>
2023-03-02 21:09:59 +0100

    update copyright

--- lib/src/main/java/com/github/tonivade/claudb/replication/SlaveReplication.java
+++ lib/src/main/java/com/github/tonivade/claudb/replication/SlaveReplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2022, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Copyright (c) 2015-2023, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
  * Distributed under the terms of the MIT License
  */
 package com.github.tonivade.claudb.replication;
