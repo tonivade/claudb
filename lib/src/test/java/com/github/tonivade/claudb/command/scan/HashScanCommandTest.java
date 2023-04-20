c8d50b20596f4aaff8eae467320c66a913e3a904
Antonio Muñoz <antoniogmc@gmail.com>
2023-04-20 23:54:23 +0200

    add header

--- lib/src/test/java/com/github/tonivade/claudb/command/scan/HashScanCommandTest.java
+++ lib/src/test/java/com/github/tonivade/claudb/command/scan/HashScanCommandTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2015-2023, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Distributed under the terms of the MIT License
+ */
 package com.github.tonivade.claudb.command.scan;
 
 import static com.github.tonivade.claudb.data.DatabaseValue.entry;
