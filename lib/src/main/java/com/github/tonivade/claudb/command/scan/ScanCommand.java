0e9d27706b6e7b8a52c470055379dbf187a4998e
Antonio Muñoz <antoniogmc@gmail.com>
2023-04-20 23:52:52 +0200

    add tests

--- lib/src/main/java/com/github/tonivade/claudb/command/scan/ScanCommand.java
+++ lib/src/main/java/com/github/tonivade/claudb/command/scan/ScanCommand.java
@@ -8,8 +8,6 @@ import static com.github.tonivade.resp.protocol.RedisToken.array;
 import static com.github.tonivade.resp.protocol.RedisToken.string;
 import static java.util.stream.Collectors.toList;
 import com.github.tonivade.claudb.command.DBCommand;
-import com.github.tonivade.claudb.command.annotation.ParamType;
-import com.github.tonivade.claudb.data.DataType;
 import com.github.tonivade.claudb.data.Database;
 import com.github.tonivade.resp.annotation.Command;
 import com.github.tonivade.resp.annotation.ParamLength;
@@ -19,20 +17,18 @@ import java.util.List;
 
 @Command("scan")
 @ParamLength(1)
-@ParamType(DataType.STRING)
 public class ScanCommand implements DBCommand {
 
   @Override
   public RedisToken execute(Database db, Request request) {
     int cursor = Integer.parseInt(request.getParam(0).toString());
     List<RedisToken> result = db.entrySet().stream()
-      .filter(entry -> entry.getValue().getType() == DataType.STRING)
       .skip(cursor).limit(10)
       .map(entry -> entry.getKey().getValue())
       .map(RedisToken::string)
       .collect(toList());
     if (result.isEmpty()) {
-      return array(string("0"));
+      return array(string("0"), array());
     }
     return array(string(String.valueOf(cursor + result.size())), array(result));
   }
