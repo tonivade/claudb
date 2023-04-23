455a945da5e98da58e53f425d2b7c21a45f34013
Antonio Muñoz <antoniogmc@gmail.com>
2023-04-23 15:34:47 +0200

    add count parameter

--- lib/src/main/java/com/github/tonivade/claudb/command/scan/ScanCommand.java
+++ lib/src/main/java/com/github/tonivade/claudb/command/scan/ScanCommand.java
@@ -7,6 +7,9 @@ package com.github.tonivade.claudb.command.scan;
 import static com.github.tonivade.resp.protocol.RedisToken.array;
 import static com.github.tonivade.resp.protocol.RedisToken.string;
 import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+
 import com.github.tonivade.claudb.command.DBCommand;
 import com.github.tonivade.claudb.data.Database;
 import com.github.tonivade.claudb.glob.GlobPattern;
@@ -14,21 +17,25 @@ import com.github.tonivade.resp.annotation.Command;
 import com.github.tonivade.resp.annotation.ParamLength;
 import com.github.tonivade.resp.command.Request;
 import com.github.tonivade.resp.protocol.RedisToken;
-import java.util.List;
 
 @Command("scan")
 @ParamLength(1)
 public class ScanCommand implements DBCommand {
 
+  private static final int DEFAULT_COUNT = 10;
+  private static final String COUNT = "count";
+  private static final String MATCH = "match";
+
   @Override
   public RedisToken execute(Database db, Request request) {
     int cursor = Integer.parseInt(request.getParam(0).toString());
+    
+    GlobPattern pattern = parsePattern(request);
+    int count = parseCount(request);
 
-    GlobPattern pattern = request.getOptionalParam(1).map(Object::toString).map(GlobPattern::new)
-      .orElse(null);
     List<RedisToken> result = db.entrySet().stream()
       .filter(entry -> pattern == null || pattern.match(entry.getKey().toString()))
-      .skip(cursor).limit(10)
+      .skip(cursor).limit(count)
       .map(entry -> entry.getKey().getValue())
       .map(RedisToken::string)
       .collect(toList());
@@ -36,5 +43,27 @@ public class ScanCommand implements DBCommand {
       return array(string("0"), array());
     }
     return array(string(String.valueOf(cursor + result.size())), array(result));
+  }
+
+  private int parseCount(Request request) {
+    int count = DEFAULT_COUNT;
+    for (int i = 1; i < request.getLength(); i++) {
+      if (request.getParam(i).toString().equalsIgnoreCase(COUNT)) {
+        count = request.getOptionalParam(++i).map(Object::toString).map(Integer::parseInt)
+          .orElse(DEFAULT_COUNT);
+      }
+    }
+    return count;
+  }
+
+  private GlobPattern parsePattern(Request request) {
+    GlobPattern pattern = null;
+    for (int i = 1; i < request.getLength(); i++) {
+      if (request.getParam(i).toString().equalsIgnoreCase(MATCH)) {
+        pattern = request.getOptionalParam(++i).map(Object::toString).map(GlobPattern::new)
+          .orElse(null);
+      }
+    }
+    return pattern;
   }
 }
--- lib/src/test/java/com/github/tonivade/claudb/command/scan/ScanCommandTest.java
+++ lib/src/test/java/com/github/tonivade/claudb/command/scan/ScanCommandTest.java
@@ -8,18 +8,21 @@ import static com.github.tonivade.claudb.data.DatabaseValue.set;
 import static com.github.tonivade.claudb.data.DatabaseValue.string;
 import static com.github.tonivade.resp.protocol.RedisToken.array;
 import static com.github.tonivade.resp.protocol.SafeString.safeString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
-import com.github.tonivade.claudb.command.CommandRule;
-import com.github.tonivade.claudb.command.CommandUnderTest;
-import com.github.tonivade.resp.protocol.RedisToken;
-import com.github.tonivade.resp.protocol.RedisTokenType;
+
 import java.util.Iterator;
-import com.github.tonivade.resp.protocol.AbstractRedisToken.ArrayRedisToken;
-import com.github.tonivade.resp.protocol.AbstractRedisToken.StringRedisToken;
+
 import org.junit.Rule;
 import org.junit.Test;
+
+import com.github.tonivade.claudb.command.CommandRule;
+import com.github.tonivade.claudb.command.CommandUnderTest;
+import com.github.tonivade.resp.protocol.AbstractRedisToken.ArrayRedisToken;
+import com.github.tonivade.resp.protocol.AbstractRedisToken.StringRedisToken;
+import com.github.tonivade.resp.protocol.RedisToken;
+import com.github.tonivade.resp.protocol.RedisTokenType;
 
 @CommandUnderTest(ScanCommand.class)
 public class ScanCommandTest {
@@ -55,7 +58,29 @@ public class ScanCommandTest {
       .withData("a", string("a"))
       .withData("ab", string("b"))
       .withData("s", set(safeString("1"), safeString("2"), safeString("3")))
-      .withParams("0", "a*")
+      .withParams("0", "match", "a*")
+      .execute()
+      .getResponse();
+
+    assertThat(response.getType(), equalTo(RedisTokenType.ARRAY));
+
+    ArrayRedisToken array = (ArrayRedisToken) response;
+
+    Iterator<RedisToken> iterator = array.getValue().iterator();
+    StringRedisToken cursor = (StringRedisToken) iterator.next();
+    ArrayRedisToken result = (ArrayRedisToken) iterator.next();
+
+    assertThat(cursor.getValue(), equalTo(safeString("2")));
+    assertThat(result.getValue(), containsInAnyOrder(RedisToken.string("a"), RedisToken.string("ab")));
+  }
+
+  @Test
+  public void withCount() {
+    RedisToken response = rule
+      .withData("a", string("a"))
+      .withData("ab", string("b"))
+      .withData("s", set(safeString("1"), safeString("2"), safeString("3")))
+      .withParams("0", "count", "2")
       .execute()
       .getResponse();
 
