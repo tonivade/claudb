aa9f5d8a6e9bd07f91153426f0c5b72bcf49ae54
Antonio Muñoz <antoniogmc@gmail.com>
2024-05-05 16:38:14 +0200

    update copyright

--- lib/src/test/java/com/github/tonivade/claudb/command/string/MultiGetCommandTest.java
+++ lib/src/test/java/com/github/tonivade/claudb/command/string/MultiGetCommandTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2023, Antonio Gabriel Muñoz Conejo <antoniogmc at gmail dot com>
+ * Copyright (c) 2015-2024, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
  * Distributed under the terms of the MIT License
  */
 package com.github.tonivade.claudb.command.string;
