567aa05f56477c907331c55eb7df7813297ddab2
Antonio Muñoz <antoniogmc@gmail.com>
2025-04-26 15:18:51 +0200

    update copyright notice

--- lib/src/main/java/com/github/tonivade/claudb/command/bitset/BitCountCommand.java
+++ lib/src/main/java/com/github/tonivade/claudb/command/bitset/BitCountCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2024, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
+ * Copyright (c) 2015-2025, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
  * Distributed under the terms of the MIT License
  */
 package com.github.tonivade.claudb.command.bitset;
